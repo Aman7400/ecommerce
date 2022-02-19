52fff3a13d4f15d1c699ae6c0b716dfbb22833dc
Aman Shukla <amanks7400@gmail.com>
2022-02-19 12:49:10 +0530

    Password length Fix

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -25,7 +25,7 @@ const schema = yup
     password: yup
       .string()
       .required("Password is required")
-      .min(4, "Password must be at least 4 characters"),
+      .min(6, "Password must be at least 6 characters"),
   })
   .required();
 
